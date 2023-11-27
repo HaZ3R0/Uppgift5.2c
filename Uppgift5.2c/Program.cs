d519ae7e2f56ef6f5cad8306a8a4119255ca2b25
HaZ3R0 <megamackan@outlook.com>
2023-11-27 21:08:14 +0100

    Add project files.

---
+++ Uppgift5.2c/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace uppgift5._2c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många tal vill du skriv in?");
+            string antaltext = Console.ReadLine();
+            int antal = int.Parse(antaltext);
+            string[] tal = new string[antal];
+            for (int i = 0; i < antal; i++)
+            {
+                Console.WriteLine("Skriv in ett tal");
+                tal[i] = Console.ReadLine();
+            }
+            int count  = 0;
+            for (int i = 0;i < antal; i++)
+            {
+                count++;
+                Console.WriteLine("tal " + count + ": " + tal[i]);
+            }
+        }
+    }
+}
